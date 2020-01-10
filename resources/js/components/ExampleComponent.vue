ee4aeb153b108e206fd3cc49302589dee8946456
phaneth07 <phanethsorn@gmail.com>
2020-01-10 13:44:15 +0700

    Initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app>
+        <v-btn small color="primary">Primary</v-btn>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
